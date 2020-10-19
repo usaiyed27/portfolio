54b9667561a217f084b8011a5384c6bff72cba79
Urusa <usaiyed.9127@gmail.com>
2020-10-18 19:10:01 -0600

    background color change feature

---
+++ backgroundChange.js
@@ -0,0 +1,31 @@
+
+window.addEventListener("keypress", function(event){
+	event.preventDefault();
+	let colorArray = ['#00A6A6', '#880044', '#2c4251', '#201E50', '3F292B', '#006D77'];
+	let currentValue = '';
+		let arrayIndex = Math.floor(Math.random() * colorArray.length);
+			color = colorArray[arrayIndex];
+			backgroundColor = '';
+			if(currentValue != arrayIndex){
+				if(backgroundColor != color){
+				document.getElementsByTagName('BODY')[0].style.backgroundColor = color;
+				backgroundColor = color;
+				}
+			}else if (currentValue == arrayIndex){
+				console.log('newIndex', arrayIndex);
+				document.getElementsByTagName('BODY')[0].style.backgroundColor = '#293132';
+			}	
+
+				currentValue = arrayIndex;
+					
+				
+})
+
+
+
+
+// document.addEventListener("keypress", function(event) {
+//   if (event.keyCode == 13) {
+//     alert('hi.');
+//   }
+// })
