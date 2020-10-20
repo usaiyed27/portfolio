8c6fd9c30aeb1bc5eaf5d3747b82dd58f8408d62
Urusa <usaiyed.9127@gmail.com>
2020-10-20 14:22:11 -0600

    project section update

--- projects.js
+++ projects.js
@@ -1,68 +1,102 @@
-// let projects = {
-// 		Project1 : {
-// 		'title' : 'Quinones Build'
-// 		},
-// 		Project2 : {
-// 		'title' : 'La Casa'
-// 		// 'description' : 'adhaliahvrai',
-// 		// 'website' : 'https://www.quinonesdesignbuild.com', 
-// 		// 'gitHub' : ''
-// 		}
-// }
+let projects = [
+		{
+		'image' : 'quinones.png',
+		'title' : 'Quinones Build',
+		'description' : 'Quinones Build Description',
+		'website' : 'https://www.quinonesdesignbuild.com', 
+		'gitHub' : ''
+		},
+		{
+		'image' : 'lacasa.png',	
+		'title' : 'La Casa',
+		'description' : 'La Casa Description',
+		'website' : 'https://www.quinonesdesignbuild.com', 
+		'gitHub' : ''
+		},
+		{
+		'image' : 'lacasa.png',	
+		'title' : 'La Casa',
+		'description' : 'La Casa Description',
+		'website' : 'https://www.quinonesdesignbuild.com', 
+		'gitHub' : ''
+		}
+
+];
+
+window.onload = mainProjectCard;
+
+function createProjectImage(obj, proDiv){
+	const projectImage = document.createElement('img');
+	projectImage.setAttribute('src', obj.image);
+	proDiv.appendChild(projectImage);
+}
+
+function createWebsiteLink(obj,proDiv){
+	const webLink = document.createElement('a');
+	webLink.className = 'btn link';
+	webLink.setAttribute('href', obj.website );
+	webLink.setAttribute('target', '_blank');
+	webLink.className = 'btn';
+	proDiv.querySelector('.card-body').appendChild(webLink);
+
+	const linkIcon= document.createElement('i');
+	linkIcon.className = 'fas fa-globe';
+	webLink.appendChild(linkIcon);
+}
+
+function createProjectDescription(obj,proDiv){
+	const projectDescription = document.createElement('p');
+	let descriptionText = document.createTextNode(obj.description);
+	projectDescription.appendChild(descriptionText);
+	projectDescription.className = 'card-text'
+	proDiv.querySelector('.card-body').appendChild(projectDescription);
+}
+
+function createProjectTitle(obj,proDiv){
+	const projectTitle= document.createElement('h5');
+	let titleText = document.createTextNode(obj.title);
+	projectTitle.appendChild(titleText);
+	projectTitle.className = 'card-title';
+	proDiv.querySelector('.card-body').appendChild(projectTitle);
+}
 
 
-// window.onload = mainProjectCard;
+function createProjectCol(i) {
+	const newCol = document.createElement('div');
+	newCol.className = 'col-6';
+	document.getElementById('project-row').appendChild(newCol);
 
+	const newCard = document.createElement('div');
+	newCard.className = 'card';
+	let project = 'project#' + i;
+	
+	$(newCard).attr({
+		 id : project,
+		'data-aos' : 'fade-up',
+		'data-aos-easing' : 'linear'
+	});
 
-// // function createProjectImage(){
-// // 	const projectImage = document.createElement('img');
-// // 	projectImage.setAttribute('src', projects[0].image);
-// // 	document.getElementsByClassName('card')[0].appendChild(projectImage);
-// // }
+	newCol.appendChild(newCard);
+}
 
-// // function createprojectLinkIcon(){
-// // 	const linkIcon= document.createElement('i');
-// // 	linkIcon.className = 'fas fa-globe';
-// // 	document.getElementsByClassName('link')[0].appendChild(linkIcon);
-// // }
-// // function createWebsiteLink(){
-// // 	const webLink = document.createElement('a');
-// // 	webLink.className = 'btn link';
-// // 	webLink.setAttribute('href', projects[0].website );
-// // 	webLink.setAttribute('target', '_blank');
-// // 	document.getElementsByClassName('card')[0].appendChild(webLink);
-// // }
+function createProjectCardBody(proDiv){
+	const newCardBody = document.createElement('div');
+	newCardBody.className = 'card-body';
+	proDiv.appendChild(newCardBody);
+}
 
-// function createProjectTitle(){
-// 	const projectTitle= document.createElement('h5');
-// 	let titleText = document.createTextNode(projects[key].title);
-// 	projectTitle.appendChild(titleText);
-// 	document.getElementsByClassName('card')[0].appendChild(projectTitle);
-// }
+function mainProjectCard(){
 
-// // function createProjectDescription(){
-// // 	const projectDescription = document.createElement('p');
-// // 	let descriptionText = document.createTextNode(projects[0].description);
-// // 	projectDescription.appendChild(descriptionText);
-// // 	document.getElementsByClassName('card')[0].appendChild(projectDescription);
-// // }
-
-// function createProjectCol() {
-// 	const newCol = document.createElement('div');
-// 	newCol.className = 'col-6 card';
-// 	// let newText = document.createTextNode("It's Working");
-// 	// newCol.appendChild(newText);
-// 	document.getElementById('project-row').appendChild(newCol);
-// }
-
-function mainProjectCard(projects){
-	// for(let x in projects){
-	// 	createProjectCol();
-	// 	//createProjectImage();
-	// 	 createProjectTitle(x);
-	// 	// createProjectDescription();
-	// 	// createWebsiteLink();
-	// 	// createprojectLinkIcon();
-	// 	console.log(x);
-	// };
+	for(let i = 0; i< projects.length; i++){
+		 let obj = projects[i];
+		createProjectCol(i);
+		let proDiv = document.getElementById('project#' + i);
+		createProjectImage(obj,proDiv);
+		createProjectCardBody(proDiv);
+		createProjectTitle(obj,proDiv);
+		createProjectDescription(obj,proDiv);
+		createWebsiteLink(obj,proDiv);
+		// createprojectLinkIcon(obj,i);
+		console.log(obj);
+	};
 }
